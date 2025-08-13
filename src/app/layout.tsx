f62f199601bf65a36016ce1219e15e311f23a053
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-08-13 16:04:26 +0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
+
+const geistSans = FontSans({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Lubb - AI-powered PDF summarizer",
+  description:
+    "Save hours of reading time. Transform Lengthy PDFs into clear, accurate summeries in seconds with our advanced AI technology.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable}antialiased`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Header />
+      <Button className="bg-blue-500 text-white hover:bg-blue-600">
+        Get Started
+      </Button>
+    </div>
+  );
+}
