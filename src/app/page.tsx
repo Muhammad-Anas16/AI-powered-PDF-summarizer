1ba04b1035ccf20488017fdc3798d28ddb2b1332
Muhammad-anas16 <anas.hanf38@gmail.com>
2025-08-14 08:05:35 -0700

    commiting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BgGradient from "@/components/common/BgGradient";
 import CTA_Section from "@/components/Home/CTA_Section";
 import DemoSection from "@/components/Home/DemoSection";
 import HeroSection from "@/components/Home/HeroSection";
@@ -7,12 +8,16 @@ import PricingSection from "@/components/Home/PricingSection";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="relative w-full">
+      {/* <BgGradient> */}
+      {/* <div className="flex flex-col"> */}
       <HeroSection />
       <DemoSection />
+      {/* </div> */}
       <HowItWorkSection />
       <PricingSection />
       <CTA_Section />
+      {/* </BgGradient> */}
     </div>
   );
 }
