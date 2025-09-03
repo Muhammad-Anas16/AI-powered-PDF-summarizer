b489588536edcccc57e9263056c90e7a2dd510a6
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-09-03 23:13:36 +0500

    update footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="bg-gray-900 dark:bg-gray-900">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 cursor-pointer">
+            © created by Muhammad Anas.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0 space-x-5">
             <a href="https://www.facebook.com/MuhammadAnasDev" className="text-gray-500 hover:text-blue-600">
